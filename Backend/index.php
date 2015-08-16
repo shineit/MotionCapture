5a989f2f2685549ef899ac1266dafa3b3a44a04d
Jessica Yeh <jyeh@smu.edu>
2015-08-15 21:23:57 -0500

    backend script for getting JSON of image list

---
+++ Backend/index.php
@@ -0,0 +1,26 @@
+<?php
+require 'vendor/autoload.php';
+
+$app = new \Slim\Slim();
+
+// [ { "name": "image-1234567890.jpg", "epochTime": 1234567890} ]
+$app->get('/images', function () {
+    $files = scandir(getcwd());
+    $pattern = '/^image-\d{10}.jpg$/';
+    $images = array();
+
+    foreach ($files as $fileName) {
+        if (preg_match($pattern, $fileName)) {
+            $epochTime = intval(substr($fileName, 6, 10));
+            $image = array("name" => $fileName, "epochTime" => $epochTime);
+            array_push($images, $image);
+        }
+    }
+
+    echo json_encode($images);
+
+});
+
+$app->run();
+
+?>
