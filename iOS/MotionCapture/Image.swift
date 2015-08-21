b9dbeee59e940f7430bb329466ee6f2a9bcccd63
Jessica Yeh <jyeh@smu.edu>
2015-08-20 21:32:15 -0500

    fixed singular/plural time since

--- iOS/MotionCapture/Image.swift
+++ iOS/MotionCapture/Image.swift
@@ -14,33 +14,46 @@ class Image {
     var url: NSURL {
         return NSURL(string: "\(Constants.hostname)/\(name)")!
     }
+    
     var thumbName: String
     var thumbUrl: NSURL {
         return NSURL(string: "\(Constants.hostname)/\(thumbName)")!
     }
+    
     var epochTime: Double
+    
     var formattedTime: String {
         let date = NSDate(timeIntervalSince1970: epochTime)
         var dateFormatter = NSDateFormatter()
         dateFormatter.dateFormat = "eee h:mm:ss a" // Mon 5:12:34 PM
         return dateFormatter.stringFromDate(date)
     }
+    
     var timeSince: String {
         let seconds = Int(NSDate().timeIntervalSince1970 - epochTime)
+        var value: Int!, unit: String!
         switch seconds {
         case 0..<60:
-            return "\(seconds) seconds ago"
+            value = seconds
+            unit = (value == 1) ? "second" : "seconds"
         case 60..<3600:
-            return "\(seconds/60) minutes ago"
+            value = seconds/60
+            unit = (value == 1) ? "minute" : "minutes"
         case 3600..<86400:
-            return "\(seconds/3600) hours ago"
+            value = seconds/3600
+            unit = (value == 1) ? "hour" : "hours"
         case 86400..<2592000:
-            return "\(seconds/86400) days ago"
+            value = seconds/86400
+            unit = (value == 1) ? "day" : "days"
         case 2592000..<31536000:
-            return "\(seconds/2592000) months ago"
+            value = seconds/2592000
+            unit = (value == 1) ? "month" : "months"
         default:
-            return "\(seconds/31536000) years ago"
+            value = seconds/31536000
+            unit = (value == 1) ? "year" : "years"
         }
+        
+        return "\(value) \(unit) ago"
     }
     
     init(name: String, thumbName: String, epochTime: Double) {
