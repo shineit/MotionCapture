5ce8c73411961278595b6ef332026fd69dc30568
Jessica Yeh <jyeh@smu.edu>
2015-08-25 16:51:15 -0500

    changed getCaptures to only add to images JSON if both the image and thumbnail are present

--- Web/getCaptures.php
+++ Web/getCaptures.php
@@ -12,32 +12,40 @@ function getJsonForImages($limit = -1) {
     $files = scandir($dir);
 
     // Filter list of files down to only images matching the correct name format
-    $filteredSortedFiles = array();
-    $pattern = '/^image-\d{13}.jpg$/';
+    $filteredImages = array();
+    $filteredThumbs = array();
+    $patternImage = '/^image-\d{13}.jpg$/';
+    $patternThumb = '/^thumb-\d{13}.jpg$/';
     foreach ($files as $fileName) {
-        if (preg_match($pattern, $fileName)) {
+        if (preg_match($patternImage, $fileName)) {
             // Key/value pairs of file name and modified time
-            $filteredSortedFiles[$fileName] = filemtime($dir . '/' . $fileName);
+            $filteredImages[$fileName] = filemtime($dir . '/' . $fileName);
+        } else if (preg_match($patternThumb, $fileName)) {
+            // Key/value pairs of file name and modified time
+            $filteredThumbs[$fileName] = filemtime($dir . '/' . $fileName);
         }
     }
 
-    // Sort the filtered list of files by modified time, descending
-    arsort($filteredSortedFiles);
-    $filteredSortedFiles = array_keys($filteredSortedFiles);
+    // Sort the filtered list of images by modified time, descending
+    arsort($filteredImages);
+    $sortedImages = array_keys($filteredImages);
+    arsort($filteredThumbs);
+    $sortedThumbs = array_keys($filteredThumbs);
 
     // Extract the epoch time from the file name, and only keep maxnum images
     $images = array();
     $imageCount = 0;
-    foreach ($filteredSortedFiles as $fileName) {
+    foreach ($sortedImages as $imageName) {
         if ($limit != -1 && $imageCount >= $limit) {
             break;
         }
-        $epochTime = substr($fileName, 6, 13);
-        // TODO: Also check for the thumbnail file, and only push image objects 
-        // that have both, so that the thumbnail name isn't hardcoded
-        $image = array("name" => "capture/".$fileName, "thumb" => "capture/thumb-".$epochTime.".jpg", "epochTime" => $epochTime);
-        array_push($images, $image);
-        $imageCount++;
+        $epochTime = substr($imageName, 6, 13);
+        $thumbName = "thumb-".$epochTime.".jpg";
+        if ($filteredThumbs[$thumbName]) {
+            $image = array("name" => "capture/".$imageName, "thumb" => "capture/".$thumbName, "epochTime" => $epochTime);
+            array_push($images, $image);
+            $imageCount++;
+        }
     }
 
     return json_encode($images);
